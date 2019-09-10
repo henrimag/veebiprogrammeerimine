2c00aafeb7699993ca5015c557a589cdb75922c8
Henri Mägi <henrimag@users.noreply.github.com>
2019-09-10 11:09:17 +0300

    esimene üleslaadimine

---
+++ 02_tund/my_index.php
@@ -0,0 +1,20 @@
+<?php
+//echo "Hei!";
+  phpversion();
+?>
+
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>Andrus Rinde programmeerib veebi</title>
+
+</head>
+<body>
+  <h1>Andrus Rinde, veebiprogrammeerimine</h1>
+  <p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+  <hr>
+  <p>PHP serveri konfiguratsiooni infot näeb <a href="serverinfo.php">SIIN</a>.</p>
+  <p>Minu esimene programmeeritud leht on <a href="testpage.php">siin</a>.</p>
+</body>
+</html>
