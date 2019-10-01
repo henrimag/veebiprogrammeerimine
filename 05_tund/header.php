965c6974141ae75f39b2f3f96e15315db97fa9e6
Henri Mägi <henrimag@users.noreply.github.com>
2019-10-01 11:48:11 +0300

    kasutaja lisamine andmebaasi

---
+++ 05_tund/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>
+  <?php
+    echo $userName;
+  ?>
+   programmeerib veebi</title>
+
+</head>
+<body>
+
+
+</body>
+</html>
+
